73fec8c68c8b3f8277a83223ad9debf92274ff1c
Arghya (ASRA) <67339217+sarkarghya@users.noreply.github.com>
2022-12-30 09:42:06 +0800

    initial commit in js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
+import DragAndDrop from './Components/DragAndDrop';
 
 function App() {
   return (
     <div className="App">
+      <DragAndDrop />
     </div>
   );
 }
