6760dd922f14fcc825e38567819471a528bb2125
Arghya Sarkar <sarkarghya@Arghyas-MacBook-Air.local>
2023-02-05 12:32:21 +0800

    note container

---
+++ src/Components/NoteContainer/NoteContainer.jsx
@@ -0,0 +1,21 @@
+import React, { useState, useRef } from 'react';
+import { Rect, Text, Group } from 'react-konva';
+
+export const NoteContainer = ({
+    x,
+    y,
+    containerProps,
+    containerNodes 
+}) => {
+    
+    return (
+    <Group
+    x={x}
+    y={y}
+    >
+        <Rect 
+        
+        />
+    </Group>
+    )
+}
